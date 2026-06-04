03041efe92f45d7806e575a5a2d43a3a8c69896d
shawnatak926 <poomepoo90@gmail.com>
2026-06-04 14:42:04 +0900

    feat : 반별 무료나눔 등록 기능 및 대시보드 순위 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import { NotFoundPage } from './pages/NotFoundPage'
 import { ProductDetailPage } from './pages/ProductDetailPage'
 import { SellPage } from './pages/SellPage'
 
+//이 부분은 App 컴포넌트입니다. 라우팅과 레이아웃을 설정하는 곳이에요. 각 페이지 컴포넌트를 라우트로 연결해주고, ProductsProvider로 감싸서 제품 관련 상태를 관리할 수 있도록 했어요. 필요에 따라 페이지를 추가하거나 수정할 수 있습니다. 
+
 function App() {
   return (
     <BrowserRouter>
