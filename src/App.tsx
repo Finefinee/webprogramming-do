681bb8e97770e24a10a9b36da0fa5ebb75ee7bed
shawnatak926 <poomepoo90@gmail.com>
2026-06-04 14:43:22 +0900

    feat: 채팅 목록, 검색 및 상세 메시지 전송 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import { ProductDetailPage } from './pages/ProductDetailPage'
 import { SellPage } from './pages/SellPage'
 
 //이 부분은 App 컴포넌트입니다. 라우팅과 레이아웃을 설정하는 곳이에요. 각 페이지 컴포넌트를 라우트로 연결해주고, ProductsProvider로 감싸서 제품 관련 상태를 관리할 수 있도록 했어요. 필요에 따라 페이지를 추가하거나 수정할 수 있습니다. 
+// 라우터는 BrowserRouter를 사용해서 설정했고, Routes와 Route 컴포넌트를 이용해서 각 경로에 맞는 페이지를 렌더링하도록 했어요. Layout 컴포넌트는 모든 페이지에서 공통적으로 사용되는 레이아웃을 제공하는 역할을 합니다.
+
 
 function App() {
   return (
